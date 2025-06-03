d112cd5057e971a04c1e28fc93bf75ce58267430
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-03 15:02:05 +0530

    Update expenseController.js

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,7 +1,7 @@
 const Expense = require('../models/expenseModel.js');
 
 // @desc    Create a new expense
-export const createExpense = async (req, res) => {
+const createExpense = async (req, res) => {
   try {
     const newExpense = new Expense(req.body);
     const savedExpense = await newExpense.save();
@@ -12,7 +12,7 @@ export const createExpense = async (req, res) => {
 };
 
 // @desc    Get all expenses
-export const getAllExpenses = async (req, res) => {
+const getAllExpenses = async (req, res) => {
   try {
     const expenses = await Expense.find().sort({ date: -1 });
     res.status(200).json(expenses);
@@ -22,7 +22,7 @@ export const getAllExpenses = async (req, res) => {
 };
 
 // @desc    Update an expense
-export const updateExpense = async (req, res) => {
+const updateExpense = async (req, res) => {
   const { id } = req.params;
   try {
     const updated = await Expense.findByIdAndUpdate(id, req.body, { new: true });
@@ -34,7 +34,7 @@ export const updateExpense = async (req, res) => {
 };
 
 // @desc    Delete an expense
-export const deleteExpense = async (req, res) => {
+const deleteExpense = async (req, res) => {
   const { id } = req.params;
   try {
     const deleted = await Expense.findByIdAndDelete(id);
@@ -43,4 +43,12 @@ export const deleteExpense = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+};
+
+// Export all controller functions
+module.exports = {
+  createExpense,
+  getAllExpenses,
+  updateExpense,
+  deleteExpense,
 };
