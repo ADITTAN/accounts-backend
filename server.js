bc6c842f3cd1a44f7dbffd72a322984fe947a9a4
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-05 00:18:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://accounts-frontend-aww3c7gs8-adittans-projects.vercel.app/', // your Vercel domain
+  origin: 'https://accounts-frontend-aww3c7gs8-adittans-projects.vercel.app', // your Vercel domain
   credentials: true,
 }));
 
