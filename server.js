da6d2ef3142c7accb860fb4d07a32f8cafe4019f
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-06 20:56:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://192.168.242.46:3000', // your Vercel domain
+  origin: 'https://cerulean-vacherin-ce46b9.netlify.app', // your Vercel domain
   credentials: true,
 }));
 
